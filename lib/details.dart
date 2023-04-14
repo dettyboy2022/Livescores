e6797af2c92ad4a5f5b413029bf2a6e7a3ca84a7
dettyboy <dettyboy@icloud.com>
2023-04-14 12:57:15 +0100

    line up tab done

--- lib/details.dart
+++ lib/details.dart
@@ -85,10 +85,11 @@ class _DetailsTabState extends State<DetailsTab> with TickerProviderStateMixin {
     return Column(
       children: [
         TabBar(
+          unselectedLabelColor: Colors.grey,
           indicator: BoxDecoration(
               shape: BoxShape.rectangle,
               color: const Color(0xffF4A58A),
-              borderRadius: BorderRadius.circular(15)),
+              borderRadius: BorderRadius.circular(25)),
           controller: tabControll,
           tabs: const [
             Tab(
@@ -107,10 +108,114 @@ class _DetailsTabState extends State<DetailsTab> with TickerProviderStateMixin {
           child: Padding(
             padding: const EdgeInsets.all(15.0),
             child: TabBarView(
-                controller: tabControll, children: const [Text('welcome')]),
+                controller: tabControll,
+                children: const [Match(), LineUp(), H2H()]),
           ),
         ),
       ],
     );
+  }
+}
+
+class Match extends StatefulWidget {
+  const Match({super.key});
+
+  @override
+  State<Match> createState() => _MatchState();
+}
+
+class _MatchState extends State<Match> {
+  var sta = [1, 2, 3, 4, 5, 6, 7];
+  var acc = [1, 2, 3, 4, 5, 6, 7];
+  var fig = [1, 2, 3, 4, 5, 6, 7];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder: (context, index) {
+      return const ListTile(
+          // leading: Text('8'),
+          // title: Text('Shooting'),
+          // trailing: Text('12'),
+          );
+    });
+  }
+}
+
+class LineUp extends StatelessWidget {
+  const LineUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color(0xFF181829),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  'Formation',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                      color: Colors.white),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text('(4-2-3-1)',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15,
+                        color: Colors.grey)),
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: [
+                TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Arsenal',
+                      style: TextStyle(color: Colors.white),
+                    )),
+                const SizedBox(
+                  width: 10,
+                ),
+                TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Aston Villa',
+                      style: TextStyle(color: Colors.white),
+                    )),
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Expanded(
+              child: Image.asset(
+                'assets/fieldFootball.png',
+                fit: BoxFit.fill,
+                width: double.infinity,
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+          ],
+        ));
+  }
+}
+
+class H2H extends StatelessWidget {
+  const H2H({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
